db88081ae92ccf7d62e4b3d1d11c9a96f7085c4c
ajonathan <jonathan-andersson@outlook.com>
2017-10-23 21:59:56 +0200

    Add files via upload

---
+++ DSCIISFeature.ps1
@@ -0,0 +1,17 @@
+﻿# WebServer Features
+
+Configuration WinIISFeature 
+{
+    Import-DscResource –ModuleName ’PSDesiredStateConfiguration’
+    
+    Node localhost
+    {
+
+        #Install IIS
+        WindowsFeature IIS 
+        {
+            Ensure = "Present"
+            Name = "Web-Server"
+        }
+    }
+}
---
+++ DSCLinuxYumHttpd.ps1
@@ -0,0 +1,38 @@
+﻿Configuration LinuxWebYum
+{
+    Import-DSCResource -Module nx
+    
+    Node "httpd"
+    {
+        nxPackage httpdInstall
+        {
+            Name = "httpd"
+            Ensure = "Present"
+            PackageManager = "Yum"
+        }
+
+        nxService httpdService
+        {
+            Name = "httpd"
+            Controller = "systemd"
+            Enabled = $true
+            State = "Running"
+        }    
+
+        nxFile httpdFile
+        {
+            Ensure = "Present"
+            Type = "File"
+            DestinationPath = "/var/www/html/index.html"
+            Contents = '<!DOCTYPE html>
+<html>
+<head>
+<title>My DSC Linux Apache Test Page</title>
+</head>
+<body bgcolor="#00c87c">
+<h3 style="color:blue">This Httpd server and webpage is installed and configured by DSC on Linux</h3>
+</body>
+</html>'
+        }
+    }
+}
